9d0d22523b661acfa06d02123f3cf1c9456948d9
Manzede Benard <manzede@gmail.com>
2020-03-07 00:25:51 +0300

    fix rating

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -21,7 +21,7 @@ class Product extends Crud
 
     public function getProductsWithRatings()
     {
-        $query = "SELECT product.*, ratings.rating FROM products AS product LEFT JOIN ratings ON ratings.id=product.id";
+        $query = "SELECT product.*, AVG(ratings.rating) AS rating FROM products AS product LEFT JOIN ratings ON ratings.product_id=product.id GROUP BY product.id";
         $products = $this->customQuery($query);
         return $products;
     }
